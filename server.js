12c364a56221e5fa1d6a42dd61834655c3901a81
kit <dhinkit@gmail.com>
2023-05-11 21:06:31 -0600

    finished functionality

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ db.query('SELECT title FROM roles', function (err, results){
     }
 });
 
+let departmentList = [];
+db.query('SELECT name FROM departments', function (err, results){
+    for (i of results){
+        departmentList.push(i['name']);
+    }
+});
+
+
 
 inquirer
 .prompt([
@@ -61,9 +69,10 @@ inquirer
         when: (answers) => answers.action === 'Add Role'
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'roleDepartment',
         message: 'Role Department: ',
+        choices: departmentList,
         when: (answers) => answers.action === 'Add Role'
     },
     {
@@ -79,15 +88,17 @@ inquirer
         when: (answers) => answers.action === 'Add Employee'
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'employeeRole',
         message: 'Employee Role: ',
+        choices: roleList,
         when: (answers) => answers.action === 'Add Employee'
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'employeeManager',
         message: 'Employee Manager: ',
+        choices: employeesList,
         when: (answers) => answers.action === 'Add Employee'
     },
     {
@@ -153,10 +164,49 @@ function updateEmployeeRole(response){
     db.query(`SELECT id FROM roles WHERE title = '${response.updateEmployeeRole}'`, function (err, results){
         roleNum = results[0]['id'];
         db.query(`UPDATE employees SET role_id = ${roleNum} WHERE first_name = '${response.updateEmployee}'`, function (err, results){
-            console.log("Role Changed!")
+            console.log("Role Changed!");
             db.query('SELECT * FROM employees', function (err, results){
                 console.table(results);
             });
+        });
+    });
+}
+
+function addDepartment(response){
+    db.query(`INSERT INTO departments(name) VALUES ('${response.departmentName}')`, function (err, results){
+        console.log("Department Added!")
+        db.query('SELECT * FROM departments', function (err, results){
+            console.table(results);
+        });
+    });
+}
+
+function addRole(response){
+    let roleDepart;
+    db.query(`SELECT id FROM departments WHERE name = '${response.roleDepartment}'`, function (err, results){
+        roleDepart = results[0]['id'];
+        db.query(`INSERT INTO roles(title, salary, department_id) VALUES ('${response.roleName}', ${response.roleSalary}, ${roleDepart})`, function(err, results){
+            console.log("Role Added!");
+            db.query('SELECT * FROM roles', function (err, results){
+                console.table(results);
+            });
+        });
+    });
+}
+
+function addEmployee(response){
+    let employeeRoleID;
+    let employeeManagerID;
+    db.query(`SELECT id FROM roles WHERE title = '${response.employeeRole}'`, function (err, results){
+        employeeRoleID = results[0]['id'];
+        db.query(`SELECT id FROM employees WHERE first_name = '${response.employeeManager}'`, function (err, results){
+            employeeManagerID = results[0]['id'];
+            db.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('${response.employeeFirstName}', '${response.employeeLastName}', ${employeeRoleID}, ${employeeManagerID})`, function (err, results){
+                console.log("Employee Added!");
+                db.query('SELECT * FROM employees', function (err, results){
+                    console.table(results);
+                });
+            })
         });
     });
 }
